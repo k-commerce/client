ad9bd64296ad2e709d1f5cfe98b8d751e7ee83cb
kwondongwook <kwondongwook1@gmail.com>
2023-03-16 21:20:18 +0900

    FEAT : 로그인

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import auth from '@/store/auth'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    auth
+  },
+  plugins: [
+    createPersistedState()
+  ]
 })
